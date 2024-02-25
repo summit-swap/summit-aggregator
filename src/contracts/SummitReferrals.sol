5a2011390876f2bc6e4db647737a2a734eef923b
halftone-dev <halftonedev@gmail.com>
2024-02-25 17:20:01 +0100

    Update referral level requirements

--- src/contracts/SummitReferrals.sol
+++ src/contracts/SummitReferrals.sol
@@ -12,12 +12,28 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./interface/ISummitReferrals.sol";
 import "./lib/Maintainable.sol";
 
+
+
+// | Level    | Custom Benefit  | Reward  | Self Volume Required  | Referred Volume Required  | Referral Num Required   |
+// | ---      | ---             | ---     | ---                   | ---                       | ---                     |
+// | Wood     |                 | 0       | 0                     | 0                         | 0                       |
+// | Bronze   | Can refer       | 0       | 100                   | 0                         | 0                       |
+// | Silver   |                 | 4%      | 1000                  | 10000                     | 3                       |
+// | Gold     |                 | 5%      | 2000                  | 25000                     | 5                       |
+// | Platinum |                 | 7%      | 5000                  | 100000                    | 10                      |
+// | Noble    |                 | 15%     | 25000                 | 1000000                   | 25                      |
+
 contract SummitReferrals is Maintainable, ISummitReferrals {
     using SafeMath for uint256;
 
-    uint256[] public LevelPoints = [0, 100e18, 10000e18, 250000e18, 1000000e18];
-    uint256[] public LevelMult = [0, 100, 200, 300, 400];
-    uint256 public HasReferrerBonusMult = 200;
+    // Level requirements
+    uint256[] public LEVEL_REF_POINTS_REQ = [0, 0, 10000e18, 25000e18, 100000e18, 1000000e18];
+    uint256[] public LEVEL_SELF_POINTS_REQ = [0, 100e18, 1000e18, 2000e18, 5000e18, 25000e18];
+    uint256[] public LEVEL_REFS_REQ = [0, 0, 3, 5, 10, 25];
+
+    uint256[] public LEVEL_MULT_REWARD = [0, 0, 400, 500, 700, 1500];
+
+    uint256 public HasReferrerBonusMult = 400;
     mapping(address => address) public referrerOf;
 
     error SelfReferral();
@@ -32,28 +48,28 @@ contract SummitReferrals is Maintainable, ISummitReferrals {
         emit UpdatedReferrer(msg.sender, _referrer);
     }
 
-    function setLevelData(uint256[] memory _points, uint256[] memory _mult, uint256 _hasReferrerBonusMult) override public onlyMaintainer {
-      if (_points.length != _mult.length) revert LengthMismatch();
-      LevelPoints = _points;
-      LevelMult = _mult;
+    function setLevelData(uint256[] memory _refPointsReq, uint256[] memory _selfPointsReq, uint256[] memory _refsReq, uint256[] memory _multRew, uint256 _hasReferrerBonusMult) override public onlyMaintainer {
+      if (_refPointsReq.length != _selfPointsReq.length || _refPointsReq.length != _refsReq.length || _refPointsReq.length != _multRew.length) revert LengthMismatch();
+      LEVEL_REF_POINTS_REQ = _refPointsReq;
+      LEVEL_SELF_POINTS_REQ = _selfPointsReq;
+      LEVEL_REFS_REQ = _refsReq;
+      LEVEL_MULT_REWARD = _multRew;
       HasReferrerBonusMult = _hasReferrerBonusMult;
-      emit UpdatedLevelData(_points, _mult, _hasReferrerBonusMult);
+      emit UpdatedLevelData(_refPointsReq, _selfPointsReq, _refsReq, _multRew, _hasReferrerBonusMult);
     }
-
-
 
     function getReferrer(address _add) override public view returns (address) {
       return referrerOf[_add];
     }  
 
     function getReferrerLevel(uint256 _points) override public view returns (uint8) {
-      for (uint8 i = 0; i < LevelPoints.length - 1; i++) {
-        if (_points < LevelPoints[i + 1]) return i;
+      for (uint8 i = 0; i < LEVEL_REF_POINTS_REQ.length - 1; i++) {
+        if (_points < LEVEL_REF_POINTS_REQ[i + 1]) return i;
       }
-      return uint8(LevelPoints.length);
+      return uint8(LEVEL_REF_POINTS_REQ.length);
     }
 
     function getReferrerMultiplier(uint256 _points) override public view returns (uint256) {
-      return LevelMult[getReferrerLevel(_points)];
+      return LEVEL_MULT_REWARD[getReferrerLevel(_points)];
     }
 }
--- src/contracts/interface/ISummitReferrals.sol
+++ src/contracts/interface/ISummitReferrals.sol
@@ -10,10 +10,10 @@ pragma solidity ^0.8.0;
 interface ISummitReferrals {
 
   event UpdatedReferrer(address indexed _add, address indexed _referrer);
-  event UpdatedLevelData(uint256[] _points, uint256[] _mult, uint256 _hasReferrerBonusMult);
+  event UpdatedLevelData(uint256[] _refPointsReq, uint256[] _selfPointsReq, uint256[] _refsReq, uint256[] _multRew, uint256 _hasReferrerBonusMult);
 
   function setReferrer(address _referrer) external;
-  function setLevelData(uint256[] memory _points, uint256[] memory _mult, uint256 _hasReferrerBonusMult) external;
+  function setLevelData(uint256[] memory _refPointsReq, uint256[] memory _selfPointsReq, uint256[] memory _refsReq, uint256[] memory _multRew, uint256 _hasReferrerBonusMult) external;
   function getReferrer(address _add) external view returns (address);
   function getReferrerLevel(uint256 _points) external view returns (uint8);
   function getReferrerMultiplier(uint256 _points) external view returns (uint256);
