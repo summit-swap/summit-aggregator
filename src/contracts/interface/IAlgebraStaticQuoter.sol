9a9e2925e21b14cac2bdbfa15b14eff784780528
halftone-dev <halftonedev@gmail.com>
2024-03-01 10:50:37 +0100

    Add static quoter

---
+++ src/contracts/interface/IAlgebraStaticQuoter.sol
@@ -0,0 +1,28 @@
+
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+pragma abicoder v2;
+
+import '../../IUniV3likeQuoterCore.sol';
+
+struct QuoteExactInputSingleParams {
+    address tokenIn;
+    address tokenOut;
+    uint256 amountIn;
+    uint160 sqrtPriceLimitX96;
+}
+
+interface IAlgebraStaticQuoter is IUniV3likeQuoterCore {
+
+    function quoteExactInputSingle(
+        QuoteExactInputSingleParams memory params
+    ) external view returns (uint256 amountOut);
+
+    function quoteExactInput(
+        bytes memory path, 
+        uint256 amountIn
+    ) external view returns (uint256 amountOut);
+
+}
+
---
+++ src/contracts/lib/PathNoFee.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+import '@uniswap/v3-periphery/contracts/libraries/BytesLib.sol';
+
+library PathNoFee {
+    using BytesLib for bytes;
+
+    uint256 constant ADDR_SIZE = 20;
+    uint256 constant MULTIPLE_POOLS_MIN_LENGTH = 3*ADDR_SIZE;
+
+    function decodeFirstPool(
+        bytes memory path
+    ) pure internal returns (address, address) {
+        return (path.toAddress(0), path.toAddress(ADDR_SIZE));
+    }
+
+    function hasMultiplePools(
+        bytes memory path
+    ) internal pure returns (bool) {
+        return path.length >= MULTIPLE_POOLS_MIN_LENGTH;
+    }
+
+    function skipToken(bytes memory path) internal pure returns (bytes memory) {
+        return path.slice(ADDR_SIZE, path.length - ADDR_SIZE);
+    }
+
+}
+
